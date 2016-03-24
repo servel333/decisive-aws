56f0a32b2bcca111483a40b582b428cd0056d98c
Nathan Perry <nateperry333@gmail.com>
2016-03-23 20:32:45 -0400

    Added a console logger as a default logger option

---
+++ decisive-driver-dynamo/lib/console-logger.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var isFunction = function isFunction(val) {
+  return val && {}.toString.call(val) === '[object Function]';
+};
+
+// console Keys => ["log", "info", "warn", "error", "dir", "time", "timeEnd", "trace", "assert", "Console"]
+
+var ConsoleLogger = module.exports = function ConsoleLogger(){};
+
+var _log = function(args, name){
+  args = Array.prototype.slice.call(args);
+  args = args.map(function(val, i){
+    return JSON.stringify(val);
+  });
+
+  /* jshint eqnull: true */
+  if (typeof console === "undefined" || console == null) {
+    return this;
+  }
+
+  if (name && isFunction(console[name])) {
+    console[name].apply(console, args);
+    return this;
+  }
+
+  if (! isFunction(console.log)) {
+    return this;
+  }
+
+  console.log.apply(console, args);
+  return this;
+};
+
+ConsoleLogger.prototype.debug   = function(){ return _log(arguments         ); };
+ConsoleLogger.prototype.error   = function(){ return _log(arguments, 'error'); };
+ConsoleLogger.prototype.err     = function(){ return _log(arguments, 'error'); };
+ConsoleLogger.prototype.info    = function(){ return _log(arguments, 'info' ); };
+ConsoleLogger.prototype.log     = function(){ return _log(arguments         ); };
+ConsoleLogger.prototype.trace   = function(){ return _log(arguments         ); };
+ConsoleLogger.prototype.warning = function(){ return _log(arguments, 'warn' ); };
+ConsoleLogger.prototype.warn    = function(){ return _log(arguments, 'warn' ); };
+ConsoleLogger.prototype.write   = function(){ return _log(arguments         ); };
+
+// Logger.prototype.trace = function(){ return this; }; // disables #debug()
+// Logger.prototype.debug = function(){ return this; }; // disables #debug()
